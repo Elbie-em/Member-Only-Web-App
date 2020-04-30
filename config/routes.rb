08197938de4bc069ddd883f65be9c28fdaf6244b
fdezcaminero <fdezcaminero@gmail.com>
2020-04-30 11:45:24 -0400

    Refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:new, :create, :index]
   devise_for :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "posts#index"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_220225) do
+ActiveRecord::Schema.define(version: 2020_04_29_222313) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2020_04_29_220225) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.integer "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_posts_on_member_id"
   end
 
 end
