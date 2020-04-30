69705e9a56a8c394aa7d9dce75d4d3712f4e34bf
fdezcaminero <fdezcaminero@gmail.com>
2020-04-30 12:24:17 -0400

    Member Registration Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:new, :create, :index]
-  devise_for :members
+  devise_for :members, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_222313) do
+ActiveRecord::Schema.define(version: 2020_04_30_161154) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 2020_04_29_222313) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_members_on_username", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
